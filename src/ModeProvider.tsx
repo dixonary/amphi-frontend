f77b05a7453a9b9f8c18420d471fe329bbd43b37
Alex Dixon <alex@dixonary.co.uk>
2021-11-20 19:31:42 +0000

    View mode is now cached

--- src/ModeProvider.tsx
+++ src/ModeProvider.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import firebase from "firebase";
+import { serialize } from "v8";
 
 export type ModeData = {
   currentMode: Mode;
@@ -17,11 +18,36 @@ const ModeContext = React.createContext<ModeData>({
   switchMode: () => {},
 });
 
+const serializeMode = (m: Mode) => {
+  switch (m) {
+    case Mode.DEFAULT: return "default";
+    case Mode.PLAYLIST_ONLY: return "playlist-only";
+    case Mode.VIDEO_ONLY: return "video-only";
+  }
+}
+const deserializeMode = (s: any) => {
+  switch (s) {
+    case "default": return Mode.DEFAULT;
+    case "playlist-only": return Mode.PLAYLIST_ONLY;
+    case "video-only": return Mode.VIDEO_ONLY;
+    default: return Mode.DEFAULT;
+  }
+}
+
 const ModeProvider = ({ children }: any) => {
-  const [currentMode, setCurrentMode] = useState<Mode>(Mode.DEFAULT);
+
+  const initialMode = useMemo(() => {
+    return deserializeMode(localStorage.getItem('view-mode'));
+  }, []);
+  const [currentMode, setCurrentMode] = useState<Mode>(initialMode);
+
+  const setMode = useCallback((m) => {
+    setCurrentMode(m);
+    localStorage.setItem('view-mode', serializeMode(m));
+  }, [setCurrentMode]);
 
   return (
-    <ModeContext.Provider value={{ currentMode, switchMode: setCurrentMode }}>
+    <ModeContext.Provider value={{ currentMode, switchMode: setMode }}>
       {children}
     </ModeContext.Provider>
   );
